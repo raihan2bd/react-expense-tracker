8190c2fd80c4982902b44d81d4b1c51e7dd890fd
Abu Raihan <raihan2bd.official@gmail.com>
2023-01-14 10:35:20 +0600

    Change filter data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import NewExpense from "./components/NewExpense/NewExpese";
-import Expenses from "./components/Expenses/Expenses";
-import { useState } from "react";
+import NewExpense from './components/NewExpense/NewExpese';
+import Expenses from './components/Expenses/Expenses';
+import { useState } from 'react';
 
 const DUMMY_EXPENSES = [
   {
-    id: "e1",
-    title: "Toilet Paper",
+    id: 'e1',
+    title: 'Toilet Paper',
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
   {
-    id: "e3",
-    title: "Car Insurance",
+    id: 'e3',
+    title: 'Car Insurance',
     amount: 294.67,
-    date: new Date(2021, 2, 28),
+    date: new Date(2021, 9, 28),
   },
   {
-    id: "e4",
-    title: "New Desk (Wooden)",
+    id: 'e4',
+    title: 'New Desk (Wooden)',
     amount: 450,
     date: new Date(2021, 5, 12),
   },
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import ExpensesList from "./ExpensesList";
-import Card from "../UI/Card";
-import ExpensesFilter from "./ExpenseFilter";
+import ExpensesList from './ExpensesList';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpenseFilter';
 import ExpensesChart from './ExpensesChart';
-import "./Expenses.css";
+import './Expenses.css';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState('2021');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
